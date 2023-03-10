746d1f8b276fb8c5db607fc8d1dffea6c8c111e7
WiseLord <wiselord1983@gmail.com>
2023-03-10 17:04:44 +0300

    cleanup_2

--- mirror.sh
+++ mirror.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 set -eu
 
-/setup-ssh.sh
+mkdir -p ~/.ssh
+echo "$INPUT_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
 git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
 
-# NOTE: Since `post` execution is not supported for local action from './' for now, we need to
-# run the command by hand.
-/cleanup.sh mirror
+git remote remove mirror
